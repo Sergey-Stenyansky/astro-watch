a5096dc52a3e7783a9ee98f2adb8524a95badca6
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-09-05 14:57:32 +0300

    fix: динамический импорт моковых данных

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,12 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 import AppEntryComponent from "./components/AppEntry/index.tsx";
 import appEntry from "./appEntry/index.ts";
-import { startMockServiceWorker } from "./mocks/browser.ts";
 
 async function run() {
   if (process.env.NODE_ENV === "development") {
-    await startMockServiceWorker();
+    const mock = await import("./mocks/browser.ts");
+
+    await mock.startMockServiceWorker();
   }
 
   createRoot(document.getElementById("root")!).render(<AppEntryComponent appEntry={appEntry} />);

eb93b7e58a8cd201d68f6d94c39ead1ad5862431
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-09-06 13:58:51 +0300

    fix: импорты

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,8 +2,6 @@ import { http, HttpResponse } from "msw";
 import appEntry from "../appEntry";
 
 import feed from "./data/feed.json";
-import neo from "./data/neo.json";
-import browse from "./data/browse.json";
 
 export const handlers = [
   http.get(`${appEntry.baseUrl}/feed`, ({ request, params, cookies }) => {

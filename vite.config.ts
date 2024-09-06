76ba78442c11820d13e3319b0ca15b42c50c0484
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-09-06 18:07:13 +0300

    chore: в процесс сборки встроен этап линта

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
+
 import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      eslint: {
+        lintCommand: "eslint ./src",
+        useFlatConfig: true,
+      },
+      overlay: { initialIsOpen: false },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),

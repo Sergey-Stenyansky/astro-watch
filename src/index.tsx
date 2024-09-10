0b6b267f49e1d97fab0f200ffdd6483e19534c02
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-09-10 14:44:09 +0300

    chore: откат версии eslint для работы с плагином eslint-plugin-import

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,12 @@ import "./index.css";
 
 import { createRoot } from "react-dom/client";
 import App from "@/components/App";
-import RequestManager from "@/util/request";
-import deployment from "@/deployment/deployment.json";
 
 async function run() {
-  RequestManager.setBaseUrl(deployment.envConfigs[deployment.env as "development"].baseUrl);
-
   if (process.env.NODE_ENV === "development") {
     const mock = await import("@/mocks/browser");
-
     await mock.startMockServiceWorker();
   }
-
   createRoot(document.getElementById("root")!).render(<App />);
 }
 

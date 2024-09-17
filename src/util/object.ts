5a64585cb5688aec3c170c29d2805907943cf4b4
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-09-17 15:20:33 +0300

    refactor: комментарий

--- src/util/object.ts
+++ src/util/object.ts
@@ -1,6 +1,5 @@
 export function assignDefault<T extends Record<string, any>>(target: T, source: Partial<T>) {
   for (const key of Object.keys(target)) {
-    // console.log("CHECK KEY", key, target[key], source[key]);
     if (Object.prototype.hasOwnProperty.call(target, key) && source[key] !== undefined) {
       target[key as keyof T] = source[key];
     }

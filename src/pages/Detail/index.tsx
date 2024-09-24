165c031fc26dd55b5c2a87bb263b348bdc8391c8
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-09-24 10:56:28 +0300

    feat: верстка орбитальных данных астероида на детальной странице

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -15,8 +15,17 @@ import {
   Accordion,
   AccordionSummary,
   AccordionDetails,
+  Dialog,
+  Button,
+  DialogTitle,
+  AppBar,
+  Toolbar,
+  IconButton,
+  DialogContent,
 } from "@mui/material";
 import { useTranslation } from "react-i18next";
+
+import Close from "@mui/icons-material/Close";
 
 import formatDate, { DateFormat } from "@/util/date/format";
 
@@ -59,6 +68,10 @@ const ApproachDataItem = ({ item }: { item: CloseApproachDataInterface }) => {
           text={t("feed.astroObjectFields.missDistance")}
           value={Number(item.missDistance.kilometers).toFixed(2)}
         />
+        <CardCell
+          text={t("detail.orbitingBody")}
+          value={t(`orbitingBodies.${item.orbitingDody.toLocaleLowerCase()}`, item.orbitingDody)}
+        />
       </AccordionDetails>
     </Accordion>
   );
@@ -79,6 +92,8 @@ const Detail = () => {
 
   const [excludePast, setExcludePast] = useToggle(true);
   const [limitItems, setLimitItems] = useToggle(true);
+
+  const [orbitalDataOpened, toggleOrbitalData] = useToggle(false);
 
   const approachData = useMemo(() => {
     let items = data?.closeApproachData || [];
@@ -124,6 +139,12 @@ const Detail = () => {
         {data.isPotentiallyHazardous && (
           <CardCell text={t("feed.astroObjectFields.isPotentiallyHazardous")} color="error" />
         )}
+        <Spacing v={1} />
+        <Box>
+          <Button variant="text" onClick={toggleOrbitalData}>
+            {t("detail.orbitalData.word")}
+          </Button>
+        </Box>
         <Checkbox label="Только будущие" checked={excludePast} onChange={setExcludePast} />
         {approachData.items.map((item) => (
           <ApproachDataItem key={item.closeApproachDateFull} item={item} />
@@ -140,6 +161,124 @@ const Detail = () => {
             </Box>
           </>
         )}
+        <Dialog fullScreen open={orbitalDataOpened} onClose={toggleOrbitalData} scroll="body">
+          <AppBar sx={{ position: "relative" }}>
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="inherit"
+                onClick={toggleOrbitalData}
+                aria-label="close"
+              >
+                <Close />
+              </IconButton>
+              <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                {t("detail.orbitalData.word")}
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <DialogContent>
+            <CardCell text={t("detail.orbitalData.orbitId")} value={data.orbitalData.orbitId} />
+            <CardCell
+              text={t("detail.orbitalData.orbitDeterminationDate")}
+              value={data.orbitalData.orbitDeterminationDate}
+            />
+            <CardCell
+              text={t("detail.orbitalData.firstObservationDate")}
+              value={data.orbitalData.firstObservationDate}
+            />
+            <CardCell
+              text={t("detail.orbitalData.lastObservationDate")}
+              value={data.orbitalData.lastObservationDate}
+            />
+            <CardCell
+              text={t("detail.orbitalData.dataArcInDays")}
+              value={data.orbitalData.dataArcInDays}
+            />
+            <CardCell
+              text={t("detail.orbitalData.observationsUsed")}
+              value={data.orbitalData.observationsUsed}
+            />
+            <CardCell
+              text={t("detail.orbitalData.orbitUncertainty")}
+              value={data.orbitalData.orbitUncertainty}
+            />
+            <CardCell
+              text={t("detail.orbitalData.minimumOrbitIntersection")}
+              value={data.orbitalData.minimumOrbitIntersection}
+            />
+            <CardCell
+              text={t("detail.orbitalData.jupiterTisserandInvariant")}
+              value={data.orbitalData.jupiterTisserandInvariant}
+            />
+            <CardCell
+              text={t("detail.orbitalData.epochOsculation")}
+              value={data.orbitalData.epochOsculation}
+            />
+            <CardCell
+              text={t("detail.orbitalData.eccentricity")}
+              value={data.orbitalData.eccentricity}
+            />
+            <CardCell
+              text={t("detail.orbitalData.semiMajorAxis")}
+              value={data.orbitalData.semiMajorAxis}
+            />
+            <CardCell
+              text={t("detail.orbitalData.inclination")}
+              value={data.orbitalData.inclination}
+            />
+            <CardCell
+              text={t("detail.orbitalData.ascendingNodeLongitude")}
+              value={data.orbitalData.ascendingNodeLongitude}
+            />
+            <CardCell
+              text={t("detail.orbitalData.orbitalPeriod")}
+              value={data.orbitalData.orbitalPeriod}
+            />
+            <CardCell
+              text={t("detail.orbitalData.perihelionDistance")}
+              value={data.orbitalData.perihelionDistance}
+            />
+            <CardCell
+              text={t("detail.orbitalData.perihelionArgument")}
+              value={data.orbitalData.perihelionArgument}
+            />
+            <CardCell
+              text={t("detail.orbitalData.aphelionDistance")}
+              value={data.orbitalData.aphelionDistance}
+            />
+            <CardCell
+              text={t("detail.orbitalData.perihelionTime")}
+              value={data.orbitalData.perihelionTime}
+            />
+            <CardCell
+              text={t("detail.orbitalData.meanAnomaly")}
+              value={data.orbitalData.meanAnomaly}
+            />
+            <CardCell
+              text={t("detail.orbitalData.meanMotion")}
+              value={data.orbitalData.meanMotion}
+            />
+            <CardCell text={t("detail.orbitalData.equinox")} value={data.orbitalData.equinox} />
+          </DialogContent>
+          <DialogTitle sx={{ paddingTop: 0 }} variant="h6">
+            {t("detail.orbitalData.orbitClass.word")}
+          </DialogTitle>
+          <DialogContent>
+            <CardCell
+              text={t("detail.orbitalData.orbitClass.type")}
+              value={data.orbitalData.orbitClass.orbitclassType}
+            />
+            <CardCell
+              text={t("detail.orbitalData.orbitClass.description")}
+              value={data.orbitalData.orbitClass.orbitClassDescription}
+            />
+            <CardCell
+              text={t("detail.orbitalData.orbitClass.range")}
+              value={data.orbitalData.orbitClass.orbitClassRange}
+            />
+          </DialogContent>
+        </Dialog>
       </Card>
     </>
   );

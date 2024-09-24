7798ad37dd3f98dacf4e113ae07e18e7f954ecdd
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-09-24 14:29:51 +0300

    refactor: типы пропсов компонента

--- src/primitives/HideOnScroll/index.tsx
+++ src/primitives/HideOnScroll/index.tsx
@@ -2,11 +2,11 @@ import { Slide } from "@mui/material";
 
 import { useScrollTrigger } from "@mui/material";
 
-function HideOnScroll({ children }: { children?: React.ReactElement<any> }) {
+function HideOnScroll({ children }: { children: React.ReactElement<any> }) {
   const trigger = useScrollTrigger();
   return (
     <Slide appear={false} direction="down" in={!trigger}>
-      {children || <div />}
+      {children}
     </Slide>
   );
 }

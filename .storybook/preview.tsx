d355216859ad984a3e22da521629036f5db96632
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-09-27 13:14:52 +0300

    fix: провайдеры для storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,9 +2,6 @@ import React from "react";
 
 import type { Preview } from "@storybook/react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { AppNavigationProvider } from "../src/components/AppNavigation/provider";
-import AppDrawerContextProvider from "../src/components/AppDrawer/AppDrawerContextProvider";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -27,20 +24,14 @@ const preview: Preview = {
   decorators: [
     (Story) => (
       <Provider store={store}>
-        <BrowserRouter>
-          <I18nextProvider i18n={i18n}>
-            <AppNavigationProvider>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <ThemeProvider theme={theme}>
-                  <AppDrawerContextProvider>
-                    <Story />
-                    <CssBaseline />
-                  </AppDrawerContextProvider>
-                </ThemeProvider>
-              </LocalizationProvider>
-            </AppNavigationProvider>
-          </I18nextProvider>
-        </BrowserRouter>
+        <I18nextProvider i18n={i18n}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <ThemeProvider theme={theme}>
+              <Story />
+              <CssBaseline />
+            </ThemeProvider>
+          </LocalizationProvider>
+        </I18nextProvider>
       </Provider>
     ),
   ],

82f49c6b5f5981aec9aef4d87ab1feb0c4a47fed
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-09-27 13:19:55 +0300

    fix: story для AppDrawer

--- src/stories/components/AppDrawer.stories.tsx
+++ src/stories/components/AppDrawer.stories.tsx
@@ -2,6 +2,9 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import AppDrawer from "@/components/AppDrawer";
 import CommonPageHeader from "@/components/CommonPageHeader";
+import { BrowserRouter } from "react-router-dom";
+import AppDrawerContextProvider from "@/components/AppDrawer/AppDrawerContextProvider";
+import { AppNavigationProvider } from "@/components/AppNavigation/provider";
 
 const meta = {
   title: "Astro Watch/Components/AppDrawer",
@@ -13,18 +16,21 @@ const meta = {
   argTypes: {
     label: { control: "text" },
   },
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <AppNavigationProvider>
+          <AppDrawerContextProvider>
+            <CommonPageHeader title="Example" />
+            <Story />
+          </AppDrawerContextProvider>
+        </AppNavigationProvider>
+      </BrowserRouter>
+    ),
+  ],
 } satisfies Meta<typeof AppDrawer>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  decorators: [
-    (Story) => (
-      <>
-        <Story />
-        <CommonPageHeader title="Example" />
-      </>
-    ),
-  ],
-};
+export const Default: Story = {};

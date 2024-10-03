c4e15a8d39cbdf2f0286cb8c2c773ade63e31fc3
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-10-03 16:24:59 +0300

    fix: it.test для остальных тестов

--- src/util/__tests__/object.test.ts
+++ src/util/__tests__/object.test.ts
@@ -2,37 +2,18 @@ import { describe, expect, it } from "@jest/globals";
 import { assignDefault } from "../object";
 
 describe("object module", () => {
-  describe("assign default function", () => {
-    it("assigns default values from an empty source object", () => {
-      expect(assignDefault({ name: "John", age: 10 }, {})).toEqual({ name: "John", age: 10 });
-    });
-    it("assigns default values from a source object with unmatching keys", () => {
-      //@ts-ignore
-      expect(assignDefault({ name: "John", age: 10 }, { email: "test@exampe.com" })).toEqual({
-        name: "John",
-        age: 10,
-      });
-    });
-    it("assigns default values from a source object with undefined keys", () => {
-      //@ts-ignore
-      expect(assignDefault({ name: "John", age: 10 }, { name: undefined })).toEqual({
-        name: "John",
-        age: 10,
-      });
-    });
-    it("assigns default values from a source object with a null value as a default", () => {
-      //@ts-ignore
-      expect(assignDefault({ name: "John", age: 10 }, { name: null })).toEqual({
-        name: null,
-        age: 10,
-      });
-    });
-    it("assigns default values from a source object", () => {
-      //@ts-ignore
-      expect(assignDefault({ name: "John", age: 10 }, { name: "Silver", age: 25 })).toEqual({
-        name: "Silver",
-        age: 25,
-      });
-    });
-  });
+  it.each([
+    [{ name: "John", age: 10 }, {}, { name: "John", age: 10 }],
+    [{ name: "John", age: 10 }, { email: "test@exampe.com" }, { name: "John", age: 10 }],
+    [{ name: "John", age: 10 }, { name: undefined }, { name: "John", age: 10 }],
+    [{ name: "John", age: 10 }, { name: null }, { name: null, age: 10 }],
+    [
+      { name: "John", age: 10 },
+      { name: "Silver", age: 25 },
+      { name: "Silver", age: 25 },
+    ],
+  ])("assigns default values from a source object", (target, source, expected) =>
+    // @ts-ignore
+    expect(assignDefault(target, source)).toEqual(expected),
+  );
 });
--- src/util/date/__tests__/window.test.ts
+++ src/util/date/__tests__/window.test.ts
@@ -4,22 +4,10 @@ import { getDateValue, timeRange } from "../window";
 import dayjs from "dayjs";
 
 describe("time window module", () => {
-  describe("get ime window from date", () => {
-    it("gets a week after date in a regular year", () => {
-      expect(getDateValue(timeRange(dayjs("2002-02-27"), 7, "days"))).toBe("2002-02-27 2002-03-05");
-    });
-    it("gets a week before date in a regular year", () => {
-      expect(getDateValue(timeRange(dayjs("2002-03-05"), -7, "days"))).toBe(
-        "2002-02-27 2002-03-05",
-      );
-    });
-    it("gets a week after date in a leap year", () => {
-      expect(getDateValue(timeRange(dayjs("2000-02-27"), 7, "days"))).toBe("2000-02-27 2000-03-04");
-    });
-    it("gets a week before date in a leap year", () => {
-      expect(getDateValue(timeRange(dayjs("2000-03-04"), -7, "days"))).toBe(
-        "2000-02-27 2000-03-04",
-      );
-    });
-  });
+  it.each([
+    [timeRange(dayjs("2002-02-27"), 7, "days"), "2002-02-27 2002-03-05"],
+    [timeRange(dayjs("2002-03-05"), -7, "days"), "2002-02-27 2002-03-05"],
+    [timeRange(dayjs("2000-02-27"), 7, "days"), "2000-02-27 2000-03-04"],
+    [timeRange(dayjs("2000-03-04"), -7, "days"), "2000-02-27 2000-03-04"],
+  ])("get time window from date", (value, expected) => expect(getDateValue(value)).toBe(expected));
 });

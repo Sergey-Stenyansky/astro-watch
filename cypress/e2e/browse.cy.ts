e46a47e4368f83cb5d845616b83245a9d2b5f933
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-10-07 17:39:00 +0300

    test: e2e тестирование для страницы поиска астероидов

---
+++ cypress/e2e/browse.cy.ts
@@ -0,0 +1,25 @@
+describe("browse page", () => {
+  beforeEach(() => {
+    cy.visit("/browse");
+  });
+
+  it("should contain page header and content", () => {
+    cy.data("page-title").should("be.visible").data("browse-table").should("be.visible");
+  });
+
+  it("should navigate to detail page", () => {
+    cy.data("browse-asteroid-app-link")
+      .first()
+      .click()
+      .location()
+      .should((loc) => {
+        expect(loc.pathname).to.include("/detail");
+      })
+      .data("nav-back")
+      .click()
+      .location()
+      .should((loc) => {
+        expect(loc.pathname).to.include("/browse");
+      });
+  });
+});

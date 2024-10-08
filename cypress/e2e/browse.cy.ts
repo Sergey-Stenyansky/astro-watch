cf97edd65b58d4028020cfe87ba30dacff052b04
Sergey Stenyanskiy <sergeystenyansky.fortech@gmail.com>
2024-10-08 09:25:18 +0300

    test: разбиение цепочки проверок для работы теста

--- cypress/e2e/browse.cy.ts
+++ cypress/e2e/browse.cy.ts
@@ -8,14 +8,11 @@ describe("browse page", () => {
   });
 
   it("should navigate to detail page", () => {
-    cy.data("browse-asteroid-app-link")
-      .first()
-      .click()
-      .location()
-      .should((loc) => {
-        expect(loc.pathname).to.include("/detail");
-      })
-      .data("nav-back")
+    cy.data("browse-asteroid-app-link").first().click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.include("/detail");
+    });
+    cy.data("nav-back")
       .click()
       .location()
       .should((loc) => {
